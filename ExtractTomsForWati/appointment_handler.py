4cffa493e72648dfa33c6e3e3af6615ae4f07d83
Dan <d.dt.wilcox@gmail.com>
2024-12-06 19:10:07 +0200

    added more optometrists and minor format changes

--- ExtractTomsForWati/appointment_handler.py
+++ ExtractTomsForWati/appointment_handler.py
@@ -4,16 +4,15 @@ import pandas as pd
 from ._abstract_handler import AbstractHandler
 
 
-
 ### Constants and Defaults ###
 #! TODO: ensure all known junk words are provided
 REMOVE_ROWS_WITH_KEYWORDS = [
-    'C/Lens Check',
-    'Funded Accounts',
-    'Private Accounts',
-    'C/Lens Purchases',
-    'Follow up Exam',
-    'Spec Exam',
+    "C/Lens Check",
+    "Funded Accounts",
+    "Private Accounts",
+    "C/Lens Purchases",
+    "Follow up Exam",
+    "Spec Exam",
 ]
 
 #! TODO: Add in the full list of Optometrists
@@ -22,21 +21,24 @@ VALID_OPTOMETRIST_LIST = [
     "Amisha Soodyall",
     "Sandesh Srikissoon",
     "Stefan van der Westhuizen",
+    "Rubina Evans",
+    "Yasmin Vawda",
 ]
 
-#? CAN_CHANGE: These are the headings of the final dataframe, free to changeAdd in the full list of Optometrists    
+# ? CAN_CHANGE: These are the headings of the final dataframe, free to changeAdd in the full list of Optometrists
 # "Date", "Time", "Name", "Cell", "Optometrists", "Practice", "CountryCode", "CellCountry"
 
 DEFAULT_CHOSEN_HEADINGS = [
-    "CellCountry", "Name", "Optometrists", "Practice", "Date", "Time" 
+    "CellCountry",
+    "Name",
+    "Optometrists",
+    "Practice",
+    "Date",
+    "Time",
 ]
 
 
-DEFAULT_VALID_FILE_TYPES= [
-    ".xls", 
-    ".xlsx"
-]
-
+DEFAULT_VALID_FILE_TYPES = [".xls", ".xlsx"]
 
 
 @dataclass
@@ -44,23 +46,31 @@ class AppointmentHandler(AbstractHandler):
 
     # Initialize file attributes
     #! Overwrite expected attributes to be defined within child class
-    valid_file_types: list[str] = field(default_factory = lambda: DEFAULT_VALID_FILE_TYPES)
+    valid_file_types: list[str] = field(
+        default_factory=lambda: DEFAULT_VALID_FILE_TYPES
+    )
 
-    #* Initialize child class's additional default attributes defined
-    valid_optometrist_list: list[str] = field(default_factory = lambda: VALID_OPTOMETRIST_LIST)
-    remove_rows_containing_list: list[str] = field(default_factory = lambda: REMOVE_ROWS_WITH_KEYWORDS)
-    default_headings_list: list[str] = field(default_factory = lambda: DEFAULT_CHOSEN_HEADINGS)
+    # * Initialize child class's additional default attributes defined
+    valid_optometrist_list: list[str] = field(
+        default_factory=lambda: VALID_OPTOMETRIST_LIST
+    )
+    remove_rows_containing_list: list[str] = field(
+        default_factory=lambda: REMOVE_ROWS_WITH_KEYWORDS
+    )
+    default_headings_list: list[str] = field(
+        default_factory=lambda: DEFAULT_CHOSEN_HEADINGS
+    )
 
-    #* ---------------------
-    #* Transform data methods
-    #* ---------------------
+    # * ---------------------
+    # * Transform data methods
+    # * ---------------------
     def _assign_optometrist(self, x):
         """Assign optometrist to patient row in considered DataFrame"""
         if x in self.valid_optometrist_list:
             return x
         else:
-            return float("nan") #! Check if can remove np.nan and replace with None
-    
+            return float("nan")  #! Check if can remove np.nan and replace with None
+
     # @abstractmethod
     def _clean_data(self, df: pd.DataFrame | None = None):
 
@@ -68,9 +78,9 @@ class AppointmentHandler(AbstractHandler):
         self.df_raw = self._validate_dataframe("df_raw", df)
         df_raw = self.df_raw.copy()
 
-        #* Clean up Excel:
+        # * Clean up Excel:
         # Drop all empty rows and last row (page X of Y)
-        df_clean = df_raw.iloc[:-1].dropna(how='all').reset_index(drop=True)
+        df_clean = df_raw.iloc[:-1].dropna(how="all").reset_index(drop=True)
 
         # Remove all non-useful header rows (first 5)
         df_clean = df_clean.iloc[6:].reset_index(drop=True)
@@ -80,7 +90,7 @@ class AppointmentHandler(AbstractHandler):
         df_clean = df_clean[~data_mask].reset_index(drop=True)
 
         # Remove all empty columns and rename headings based on first rows cells
-        df_clean = df_clean.dropna(axis=1, how='all')
+        df_clean = df_clean.dropna(axis=1, how="all")
         df_clean.columns = df_clean.iloc[0]  # Assign row 0 to be the new headers
         df_clean = df_clean.drop(0).reset_index(drop=True)
 
@@ -88,15 +98,19 @@ class AppointmentHandler(AbstractHandler):
         dropped_columns = df_clean.columns.notna()
         df_clean = df_clean.loc[:, dropped_columns]
         df_clean = df_clean.drop(
-            columns = ["PatientNo", "Home", "Work", "Medical Aid", "Plan", "Number"]
-        )        
+            columns=["PatientNo", "Home", "Work", "Medical Aid", "Plan", "Number"]
+        )
 
         # Assign an optometrist to patient from the 'Date' column
-        df_clean["Optometrists"] = df_clean["Date"].apply(lambda x: self._assign_optometrist(x)).ffill()
+        df_clean["Optometrists"] = (
+            df_clean["Date"].apply(lambda x: self._assign_optometrist(x)).ffill()
+        )
 
         # Keep only valid dates in 'Date' Column
         df_clean["Date"] = df_clean["Date"].ffill()
-        date_mask = pd.to_datetime(df_clean["Date"], format="%Y-%m-%d", errors="coerce").notnull()
+        date_mask = pd.to_datetime(
+            df_clean["Date"], format="%Y-%m-%d", errors="coerce"
+        ).notnull()
         df_clean = df_clean[date_mask].reset_index(drop=True)
 
         # Ensure users names in 'Name' column are titled
@@ -109,7 +123,7 @@ class AppointmentHandler(AbstractHandler):
 
     # @abstractmethod
     def _add_features(self, df: pd.DataFrame | None = None):
-        
+
         # Retrieve relevant dataframe and validate
         self.df_clean = self._validate_dataframe("df_clean", df)
         df = self.df_clean.copy()
@@ -131,14 +145,12 @@ class AppointmentHandler(AbstractHandler):
         df["Date"] = pd.to_datetime(df["Date"]).dt.date
 
         # CountryCell, Patient, Optometrist, Practice, Date, Time
-        
-        # Reorder Dataframe
-        df = df.reindex(self.default_headings_list, axis = 1)
 
+        # Reorder Dataframe
+        df = df.reindex(self.default_headings_list, axis=1)
 
         self.df_clean = df
         return df
-
 
     # @abstractmethod
     def save_data(self, savepath: str | None = None):
@@ -146,24 +158,26 @@ class AppointmentHandler(AbstractHandler):
 
         # Check for file_path availability
         if not hasattr(self, "file_path"):
-            raise AttributeError("Missing 'file_path' attribute. Cannot save data unless import data filepath is available.")
+            raise AttributeError(
+                "Missing 'file_path' attribute. Cannot save data unless import data filepath is available."
+            )
 
         # Set for save_path based on availability
-        self.save_path = savepath or getattr(self, "save_path", None) or self._get_savepath_from_filepath()
-      
-        # Check output DataFrame availability 
+        self.save_path = (
+            savepath
+            or getattr(self, "save_path", None)
+            or self._get_savepath_from_filepath()
+        )
+
+        # Check output DataFrame availability
         if not hasattr(self, "df_output"):
-            raise AttributeError("Missing 'df_output' attribute. Cannot save data unless data is provided.")
-        
+            raise AttributeError(
+                "Missing 'df_output' attribute. Cannot save data unless data is provided."
+            )
+
         # Get output pd.DataFrame and save
         self.df_output.to_csv(self.save_path, index=False)
 
 
 if __name__ == "__main__":
     appointment_handler = AppointmentHandler()
-
-
-
-
-
-
